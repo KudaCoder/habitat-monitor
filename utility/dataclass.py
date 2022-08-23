59456a4a8fb9024523fda41c8b86f2615e85b87c
Warren Snowden <warren.snowden@gmail.com>
2022-08-23 17:12:22 +0100

    add to_ & from json methods for dataclasses

--- utility/dataclass.py
+++ utility/dataclass.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, asdict
 from datetime import datetime, time
+from typing import Optional
+import json
 
 
 @dataclass
@@ -14,13 +15,46 @@ class Environment:
     humidity_h_sp: Optional[float] = None
     humidity_l_sp: Optional[float] = None
 
+    def to_json(self):
+        data = asdict(self)
+        for k, v in data.items():
+            if isinstance(v, time):
+                data[k] = v.isoformat()
+        return json.dumps(data)
+    
+    @staticmethod
+    def from_json(json_data):
+        env = Environment()
+        data = json.loads(json_data)
+        for k, v in data.items():
+            if hasattr(env, k):
+                try:
+                    v = datetime.strptime(v, "%H:%M:%S").time()
+                except Exception:
+                    v = float(v)
+                finally:
+                    setattr(env, k, v)
+
+        return env
+
 
 @dataclass
 class Reading:
-    temp: Optional[float] = None
-    hum: Optional[float] = None
+    temperature: Optional[float] = None
+    humidity: Optional[float] = None
+
+    def to_json(self):
+        return json.dumps(asdict(self))
+    
+    @staticmethod
+    def from_json(json_data):
+        data = json.loads(json_data)
+        return Reading(**data)
 
 
 @dataclass
 class Light:
     turned_on: Optional[bool] = None
+
+    def to_json(self):
+        return json.dumps(asdict(self))
